af74c9c5e21c272dc3242f3db260c141e03fc689
NotHiaki <celsohenrique367@gmail.com>
2023-12-29 20:56:41 -0300

    feat: user model sequelize

---
+++ backend/models/User.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+const db = require('../db/conn');
+
+const User = db.define('User', {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = User;
