212b07ef7aeef4749561cb9c61d65da433897b5b
NotHiaki <celsohenrique367@gmail.com>
2024-01-01 15:36:38 -0300

    chore: checkUserExistence middleware

--- backend/middlewares/checkUserExistence.js
+++ backend/middlewares/checkUserExistence.js
@@ -3,21 +3,24 @@ import { user } from "../models/user.js";
 export async function checkUserExistence(req, res, next) {
     const { username, email } = req.body;
 
-    const query = await user.findOne({
-        where: {
-            username,
-            email
+    try {
+        const query = await user.findOne({
+            where: {
+                username,
+                email
+            }
+        });
+        
+        if(query) {
+            return res.status(409).json({
+                "message": "Username or email already exist."
+            });
         }
-    })
     
-    if(query) {
-        return res.status(409).json({
-            "message": "Username or email already exist."
+        next();
+    } catch (error) {
+        return res.status(500).json({
+            "message": "Internal server error."
         });
     }
-
-    next();
 }
-
-
-///add try catch
