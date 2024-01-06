b104e4f85d96ada16f664889d385fcb138c8ab7c
NotHiaki <celsohenrique367@gmail.com>
2024-01-06 00:21:28 -0300

    chore: json return message

--- backend/controllers/auth/register.js
+++ backend/controllers/auth/register.js
@@ -37,7 +37,9 @@ export async function register(req, res) {
             password: encryptPassword(password)
         });
 
-        return res.status(201);
+        return res.status(201).json({
+            "message": "User created successfully."
+        });
     } catch (error) {
         return res.status(500).json({
             "message": error.message
