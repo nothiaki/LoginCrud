02d69dc461239761fb885bf8eaff3a04d87ae0a6
NotHiaki <celsohenrique367@gmail.com>
2024-01-23 20:37:43 -0300

    feat: login page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../app/pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'register', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
     
     { path: '**', component: HomeComponent }
 ];
