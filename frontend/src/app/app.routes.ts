92634243159844a226be9763d89f995c4371c68c
NotHiaki <celsohenrique367@gmail.com>
2024-01-22 21:15:50 -0300

    feat: home component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../app/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    
+    { path: '**', component: HomeComponent }
+];
