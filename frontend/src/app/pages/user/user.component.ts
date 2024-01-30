51ad90fe3a0919a4545946195a516cfda5565c27
NotHiaki <celsohenrique367@gmail.com>
2024-01-30 15:40:38 -0300

    feat: editUser modal

--- frontend/src/app/pages/user/user.component.ts
+++ frontend/src/app/pages/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user',
@@ -9,10 +11,22 @@ import { Router } from '@angular/router';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  formGroup!: FormGroup;
   username: string | null = localStorage.getItem('username');
   email: string | null = localStorage.getItem('email');
+  showModal: boolean = false;
 
+  private formBuilder = inject(FormBuilder);
+  private userService = inject(UserService);
   private router = inject(Router);
+
+  constructor() {
+    this.formGroup = this.formBuilder.group({
+      userrname: '',
+      email: '',
+      password: ''
+    });
+  }
 
   logout(): void {
     localStorage.removeItem('token');
@@ -20,5 +34,13 @@ export class UserComponent {
     localStorage.removeItem('email');
     
     this.router.navigate(['/register']);
+  }
+
+  modalControl(): void {
+    this.showModal = !this.showModal;
+  }
+
+  editUser(): void {
+    console.log('editUser');
   }
 }
